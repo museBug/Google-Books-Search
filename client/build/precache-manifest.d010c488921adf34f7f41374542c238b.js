a781e517f8a2818de89cfb72dccce70e81b938cc
Regina Musovirova <musovirova@yahoo.com>
2020-06-16 21:59:55 -0400

    create react expres app

---
+++ client/build/precache-manifest.d010c488921adf34f7f41374542c238b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9481deafa935917ee1d2",
+    "url": "/static/css/main.b9f3237b.chunk.css"
+  },
+  {
+    "revision": "9481deafa935917ee1d2",
+    "url": "/static/js/main.be766aa2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "abc921c608ebbf2c207b",
+    "url": "/static/js/2.fdc7f0fe.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "d84440a1faae0e24d537913e0391a9a9",
+    "url": "/index.html"
+  }
+];
