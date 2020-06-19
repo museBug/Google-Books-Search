4f0e71c10cc0ff0be47725baa434e2654a299691
Regina Musovirova <musovirova@yahoo.com>
2020-06-18 22:00:48 -0400

    create seedDB.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>hello, baltimore!</h2>
-      </div>
-      <div>
-        <h1>week 21!</h1>
-      </div>
+      
+      
     </div>
   );
 }
