f712773e3a0c7e65275455bda856c5397043c624
Regina Musovirova <musovirova@yahoo.com>
2020-06-18 23:08:04 -0400

    import image to the header.js component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
+import headerImage from "../images/book.jpg"
 
 const headerStyle = {
     "backgroundImage": `url(${headerImage})`,
     "backgroundSize": "cover",
-    
+
 }
 function Header() {
     return (
         <header style={headerStyle}>
-            <h1>(React) Google Books Search</h1>
+            <h1>Google Books Search</h1>
             <p>Search for and Save Books of Interest</p>
         </header>
     );
