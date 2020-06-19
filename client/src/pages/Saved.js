6ea89d91b9dc9e58301e92b634f537dc9005e0df
Regina Musovirova <musovirova@yahoo.com>
2020-06-19 16:22:04 -0400

    finish up saved and search pages

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,12 +1,33 @@
 import React from "react";
-
+import ResultCont from "../components/ResultCont";
+import API from "../utils/API";
 
 
 class Saved extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            savedBooks: []
+        }
+    }
+
+
+    componentWillMount() {
+        API.getBooks().then(
+            (response) => {
+                this.setState({savedBooks: response.data});
+            }
+        ).catch(
+            (err) => {
+                console.log(err);
+            }
+        );
+    }
+
     render () {
         return (
             <main>
-               
+                 <ResultCont savedBooks={this.state.savedBooks} path={this.props.match.path}/>
             </main>
         )
         
